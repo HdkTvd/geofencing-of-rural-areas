1ddd499108c3627f4605cd3034923216205edc3a
Samuel <sam99dave@gmail.com>
2020-01-24 19:32:12 +0530

    Useless changes

--- server.js
+++ server.js
@@ -1,62 +1,60 @@
-var express = require('express')
-var logger = require('morgan')
-var bodyParser = require('body-parser')
+var express = require("express");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
 
-var admin = require('firebase-admin')
+var admin = require("firebase-admin");
 
 // This account is no longer valid
-var serviceAccount = require('./agriculture-db-firebase-adminsdk-h8r3y-279d91e862.json')
+var serviceAccount = require("./agriculture-db-firebase-adminsdk-h8r3y-279d91e862.json");
 
 var firebaseAdmin = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://agriculture-db.firebaseio.com"
-})
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://agriculture-db.firebaseio.com"
+});
 
-var database = firebaseAdmin.database()
+var database = firebaseAdmin.database();
 
 // Create instance of express app
-var app = express()
+var app = express();
 
 // We want to serve js and html in ejs
 // ejs stands for embedded javascript
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // We also want to send css, images, and other static files
-app.use(express.static('views'))
-app.set('views', __dirname + '/views')
+app.use(express.static("views"));
+app.set("views", __dirname + "/views");
 
 // Give the server access to the user input
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(logger('dev'))
+app.use(logger("dev"));
 
-app.get('/', function(request, response){
-    var restaurantsRef = database.ref("/restaurants")
-    
-    restaurantsRef.once('value', function(snapshot){
-        var data = snapshot.val()
-        
-        if ( !data ) {
-            data = {}
-        }
-        
-        response.render('home.ejs', { restaurants: data })        
-    })
-})
+app.get("/", function(request, response) {
+  var restaurantsRef = database.ref("/restaurants");
 
-app.get('/profile/:userId', (req, res) => {
-    var userId = req.params.userId
-    
-    var user = firebaseAdmin.getAllUsersData(userId)
-    
-    res.render('profile.ejs', { user: user })
-})
+  restaurantsRef.once("value", function(snapshot) {
+    var data = snapshot.val();
 
-var port = process.env.PORT || 8080
+    if (!data) {
+      data = {};
+    }
 
-app.listen(port, function(){
-    console.log('App running on port ' + port)
-})
+    response.render("home", { restaurants: data });
+  });
+});
 
+app.get("/profile/:userId", (req, res) => {
+  var userId = req.params.userId;
 
+  var user = firebaseAdmin.getAllUsersData(userId);
+
+  res.render("profile.ejs", { user: user });
+});
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+  console.log("App running on port " + port);
+});
